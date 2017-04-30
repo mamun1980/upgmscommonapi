c461145d46abaefaed46c00acfcc5b3564403157
K Md Mamunur Rashid <mamun1980@gmail.com>
2017-05-01 01:10:28 +0600

    updated static_root settings

--- upgmscommonapi/settings.py
+++ upgmscommonapi/settings.py
@@ -111,6 +111,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, "public")
 
 if DEBUG:
     STATICFILES_DIRS = [
